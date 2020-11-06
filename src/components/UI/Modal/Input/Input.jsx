07825d54e8556440ad56f85fe5df8144c2d3a79e
gulshanyadav01 <gulshany01@gmail.com>
2020-11-06 09:05:23 +0530

    custom input

---
+++ src/components/UI/Modal/Input/Input.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Input = (props) => { 
+    let inputElement  = null; 
+
+    switch (props.inputType) {
+        case ("input"):
+            inputElement = <input {...props}/>
+            break;
+        case ("textarea"):
+            inputElement = <input {...props}/>
+            break;
+    
+        default:
+            inputElement = <input {...props}/>;
+    }
+
+    return (
+        <div>
+            <label>{props.label}</label>
+            {inputElement}
+        </div>
+
+    )
+
+}
+
+export default Input; 
